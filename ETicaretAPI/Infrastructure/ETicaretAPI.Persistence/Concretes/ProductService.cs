159fba472fe6c18d3b84957d13b859975fcc4ef4
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-15 18:39:06 +0300

    Add E-Ticaret API project files

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new() 
+        {
+            new() {Id = Guid.NewGuid(),Name = "Product 1 ",Price = 900,Stock = 12 },
+            new() {Id = Guid.NewGuid(),Name = "Product 2 ",Price = 100,Stock = 14 },
+            new() {Id = Guid.NewGuid(),Name = "Product 3 ",Price = 1000,Stock = 10 },
+            new() {Id = Guid.NewGuid(),Name = "Product 4 ",Price = 1000,Stock = 10 },
+            new() {Id = Guid.NewGuid(),Name = "Product 5 ",Price = 300,Stock = 10 },
+            new() {Id = Guid.NewGuid(),Name = "Product 6 ",Price = 500,Stock = 10 },
+            new() {Id = Guid.NewGuid(),Name = "Product 7 ",Price = 600,Stock = 40 },
+            new() {Id = Guid.NewGuid(),Name = "Product 8 ",Price = 200,Stock = 14 },
+            new() {Id = Guid.NewGuid(),Name = "Product 9 ",Price = 100,Stock = 10 }
+
+        };
+        
+    }
+}
