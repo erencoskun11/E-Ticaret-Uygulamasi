3eb41b51cd91f0b08f42247637aacb3654e4b0c5
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-29 22:47:22 +0300

    feat(validator) : created

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,40 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("pls write something for name of product")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                .WithMessage("write correctly");
+
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("dfb")
+                .Must(s => s >= 0)
+                .WithMessage("drbg");
+
+
+            RuleFor(s => s.Price)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("dfb")
+                .Must(s => s >= 0)
+                .WithMessage("dfb");
+
+
+
+
+        }
+    }
+}
