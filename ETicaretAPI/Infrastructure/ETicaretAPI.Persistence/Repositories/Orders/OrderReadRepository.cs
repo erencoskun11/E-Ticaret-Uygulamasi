8b63129ae6484b31521684780c8c8f232875049f
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-06 10:36:10 +0300

    feat(Repository) : created for Read

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/Orders/OrderReadRepository.cs
@@ -0,0 +1,43 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.Repositories.Customers;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories.Orders
+{
+    public class OrderReadRepository : ReadRepository<Order>, IOrderReadRepository
+    {
+        public OrderReadRepository(ETicaretAPIDbContext context) : base(context) 
+        {
+            
+        }
+        DbSet<Order> IRepository<Order>.Table => throw new NotImplementedException();
+
+        public Task<Order> GetSingleAsync(Expression<Func<Order, bool>> method)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Order> GetWhere(Expression<Func<Order, bool>> method)
+        {
+            throw new NotImplementedException();
+        }
+
+        IQueryable<Order> IReadRepository<Order>.GetAll(bool tracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Order> IReadRepository<Order>.GetByIdAsync(string id, bool tracking = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
