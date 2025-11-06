4e74fffe3d9ebe9f93bfe60dca9723e935c0c7c9
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-06 10:39:26 +0300

    created

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+        static public string ConnectionString
+        {
+            get
+            {
+                var configuration = new ConfigurationManager();
+
+                // appsettings.json dosyasının yolu
+                configuration.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+                configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                // "PostgreSQL" isimli connection string'i döndür
+                return configuration.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
